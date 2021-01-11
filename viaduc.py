ec8d9bb49e43109e201f3920cfb0e81b53973950
Diego Torres Milano <dtmilano@gmail.com>
2021-01-10 18:26:56 -0800

    Add version and copyright
    - Ignore idea files

--- viaduc.py
+++ viaduc.py
@@ -1,5 +1,25 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+'''
+Copyright (C) 2021  Diego Torres Milano
+Created on Jan 2, 2021
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author: Diego Torres Milano
+'''
+
+__version__ = '1.0.0'
 
 import argparse as argparse
 import re
