b7fe74ca385dab615d17a369bbe371a5201dd22d
Diego Torres Milano <dtmilano@gmail.com>
2022-06-20 21:18:30 -0700

    Add example

---
+++ examples/no-bootstrap.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+import sys
+from viaduc import Viaduc
+
+
+class Presentation(Viaduc.Presentation):
+    width = height = 300
+    title = 'no bootstrap'
+    html = '''
+<!DOCTYPE html>
+  <head>
+    <title>{{title}}</title>
+  </head>
+  <body style="background: #de3163; color: #fbfcfc; overflow: hidden">
+    <div style="font-size: 15em; margin: auto; width: 50%;">V</div>
+  </body>  
+</html>
+'''
+
+
+if __name__ == '__main__':
+    Viaduc(presentation=Presentation(), args=sys.argv + ['--no-bootstrap'])
